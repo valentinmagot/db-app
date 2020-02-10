e5cc99c2a80fa4f530c3a59a738a37ae295a9da9
valentinmagot <valentin_magot@outlook.com>
2020-02-09 22:05:36 -0500

    Added nav bar and navigation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+// import { BrowserRouter } from 'react-router-dom';
 
 
 
