4989b05b5ad26ebc485325be5804060d6793b3fe
valentinmagot <valentin_magot@outlook.com>
2020-03-27 10:16:39 -0400

    more fix

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import {  BrowserRouter as Router  , Switch, Route} from 'react-router-dom';
 // import NavigationBar from './NavigationBar.js';
 import Data from './Components/Data.js';
 import LandingPage from './Components/LandingPage.js';
-<<<<<<< HEAD
-=======
 import PartnersPage from './Components/PartnersPage';
->>>>>>> 5d6a90baa6b585b2b12acb76a020e2c154b802a1
 
 
 class App extends Component {
@@ -27,10 +24,7 @@ class App extends Component {
       <Switch >
             <Route  path="/" exact component={LandingPage}/>
             <Route path="/Data" component={Data}/>
-<<<<<<< HEAD
-=======
             <Route path="/PartnersPage" component={PartnersPage}/>
->>>>>>> 5d6a90baa6b585b2b12acb76a020e2c154b802a1
         </Switch>
     </Router>
     );
